d8e32e8a9b140acd9a34400182a9022aee810f76
James Halliday <mail@substack.net>
2012-06-24 06:42:47 -0700

    let the command take files

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 var brake = require('../');
+var fs = require('fs');
+
 var argv = require('optimist')
     .alias('r', 'rate')
     .alias('p', 'period')
@@ -8,15 +10,26 @@ var argv = require('optimist')
     .alias('maxsize', 'maxSize')
     .argv
 ;
-if (!argv.rate) argv.rate = argv._[0];
+if (!argv.rate) argv.rate = argv._.shift();
 if (!argv.rate) {
     return fs.createReadStream(__dirname + '/usage.txt')
         .pipe(process.stderr);
     ;
 }
+if (!argv.period && /^\d+$/.test(argv._[0])) {
+    argv.period = argv._.shift();
+}
 
-process.stdin
-    .pipe(brake(argv))
-    .pipe(process.stdout)
-;
-process.stdin.resume();
+if (argv._[0]) {
+    fs.createReadStream(argv._[0])
+        .pipe(brake(argv))
+        .pipe(process.stdout)
+    ;
+}
+else {
+    process.stdin
+        .pipe(brake(argv))
+        .pipe(process.stdout)
+    ;
+    process.stdin.resume();
+}
