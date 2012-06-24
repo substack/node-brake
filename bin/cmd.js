2335dbce024f51cf50caad28a66a9a3c8e911645
James Halliday <mail@substack.net>
2012-06-24 06:33:55 -0700

    command-line usage

---
+++ bin/cmd.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+var brake = require('../');
+var argv = require('optimist')
+    .boolean('smooth')
+    .alias('r', 'rate')
+    .alias('p', 'period')
+    .alias('s', 'smooth')
+    .alias('m', 'maxSize')
+    .alias('maxsize', 'maxSize')
+    .argv
+;
+if (!argv.rate) argv.rate = argv._[0];
+if (!argv.rate) {
+    return fs.createReadStream(__dirname + '/usage.txt')
+        .pipe(process.stderr);
+    ;
+}
+
+process.stdin
+    .pipe(brake(argv))
+    .pipe(process.stdout)
+;
+process.stdin.resume();
