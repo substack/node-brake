a625e16ddb1812570f14de6ae84d6140c3b0da0d
James Halliday <mail@substack.net>
2012-06-24 03:36:04 -0700

    take muxing out of example

--- example/slow.js
+++ example/slow.js
@@ -1,46 +1,28 @@
 var Stream = require('stream');
 var throttle = require('../');
 
-var important = (function () {
-    var s = new Stream;
-    s.readable = true;
-    s.pause = function () {
-        console.log('0 PAUSE');
-    };
-    s.resume = function () {
-        console.log('0 RESUME');
-    };
-     
-    setInterval(function () {
-        s.emit('data', '1');
-    }, 100);
-    return s;
-})();
-
 var bulk = (function () {
     var s = new Stream;
     s.readable = true
     s.pause = function () {
-        console.log('1 PAUSE');
+        clearInterval(iv);
     };
     s.resume = function () {
-        console.log('1 RESUME');
+        iv = createInterval();
     };
     
-    setInterval(function () {
-        s.emit('data', '0');
-    }, 10);
+    var iv = createInterval();
+    var x = 0;
+    function createInterval () {
+        return setInterval(function () {
+            s.emit('data', String(x));
+            x = x ^ 1;
+        }, 10);
+    }
     return s;
 })();
 
-var MD = require('mux-demux');
-var md = new MD;
-console.dir(md);
-
-important.pipe(md.createWriteStream('important'));
-bulk.pipe(md.createWriteStream('bulk'));
-
-md.getMuxDemuxStream()
-    .pipe(throttle(3, { period : 100, maxSize : 1000 }))
+bulk
+    .pipe(throttle(1, 100))
     .pipe(process.stdout, { end : false })
 ;
