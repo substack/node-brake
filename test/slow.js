f88fc5351fe72fbb33224a3f3cec4ae66bba8561
James Halliday <mail@substack.net>
2012-06-24 04:33:51 -0700

    passing max size test

--- test/slow.js
+++ test/slow.js
@@ -57,3 +57,15 @@ test('10 bytes / sec', function (t) {
         s.end();
     }));
 });
+
+test('max size', function (t) {
+    t.plan(1);
+    
+    var s = createReadStream(5);
+    delete s.pause;
+    delete s.resume;
+    s.pipe(brake(10, { maxSize : 100 }).on('error', function (err) {
+        t.ok(/maximum buffer size exceeded/.test(err));
+        s.end();
+    }));
+});
